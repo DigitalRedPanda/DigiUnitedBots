297b1aa7c5836efd1fd54387caa605bf0d01c96d
DigitalRedPanda <osama-12341905@hotmail.com>
2022-01-30 15:10:27 +0300

    Merge remote-tracking branch 'gihub/master'
    # Conflicts:
    #	assets/.env

--- src/main/java/TwitchBot/Scan.java
+++ src/main/java/TwitchBot/Scan.java
@@ -1,4 +1,18 @@
 package TwitchBot;
 
 public class Scan {
+    boolean Find(String ToFind, String Context){
+        String[] SplittedContext = Context.split(" ");
+        char[] CharactersWithinContext;
+        for(String NavigateContext: SplittedContext){
+            if(NavigateContext.equalsIgnoreCase("SI")){
+                CharactersWithinContext = NavigateContext.toCharArray();
+                for(char FindCharacters: CharactersWithinContext){
+                    boolean FindFirstHalf = FindCharacters != 'S' || FindCharacters != 's';
+                    boolean FindSecondHalf = FindCharacters != 'I' || FindCharacters != 'i';
+                    if(FindFirstHalf && FindSecondHalf)
+                        return false;
+                }}}
+        return true;
+    }
 }
